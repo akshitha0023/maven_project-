27cb9ead532ec56c3dc1360eac75fa1924e4fd5b
akshitha <21bd1a664wcsmc@gmail.com>
2024-01-06 14:19:41 +0530

    pushed sucessfully

---
+++ src/main/java/SEGroup/octAkshithajava/App.java
@@ -0,0 +1,13 @@
+package SEGroup.octAkshithajava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello  31 OCT" );
+    }
+}
